3b0270f6f100b303119c5d49875346602696e20b
placiderapson <placideirandora@gmail.com>
2021-03-09 17:36:57 +0200

    Setup the Expo Google Fonts

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import { theme } from './src/infrastructure/theme';
 import RestaurantScreen from './src/features/restaurant/screens/RestaurantScreen';
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
