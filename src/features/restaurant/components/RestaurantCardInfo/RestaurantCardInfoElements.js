f668b8b9f1c39c4d787f2fb69526422d2b5e67e5
placiderapson <placideirandora@gmail.com>
2021-03-09 18:11:45 +0200

    Add Rating and Address  Details to the Restaurant Card

--- src/features/restaurant/components/RestaurantCardInfo/RestaurantCardInfoElements.js
+++ src/features/restaurant/components/RestaurantCardInfo/RestaurantCardInfoElements.js
@@ -6,6 +6,7 @@ export const Title = styled.Text`
   text-transform: ${(props) => props.theme.transform[1]};
   color: ${(props) => props.theme.colors.ui.primary};
   font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
 `;
 
 export const RestaurantCard = styled(Card)`
@@ -15,4 +16,17 @@ export const RestaurantCard = styled(Card)`
 
 export const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[0]};
+`;
+
+export const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+
+export const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
+  text-transform: ${(props) => props.theme.transform[1]};
 `;
--- src/features/restaurant/components/RestaurantCardInfo/index.js
+++ src/features/restaurant/components/RestaurantCardInfo/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import { SvgXml } from 'react-native-svg';
 
 import {
   RestaurantCard,
   RestaurantCardCover,
   Title,
+  Address,
+  Rating,
 } from './RestaurantCardInfoElements';
+import star from '../../../../../assets/star';
 
 const RestaurantCardInfo = ({ restaurant = {} }) => {
   const {
     name = 'Sundowner',
     // icon,
     photos = ['https://picsum.photos/400'],
-    // address = '50 KG 23 St',
+    address = '50 KG 23 St',
     // isOpenNow = true,
-    // rating = 4,
+    rating = 4,
     // isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  console.log('RATING ARRAY: ', ratingArray);
 
   return (
     <>
       <RestaurantCard elevation={5}>
         <RestaurantCardCover source={{ uri: photos[0] }} />
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map((_, index) => (
+            <SvgXml xml={star} width={20} height={20} key={index} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
       </RestaurantCard>
     </>
   );
