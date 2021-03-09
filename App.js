96735d934e5e374984986620776aefdfdd9c17b7
placiderapson <placideirandora@gmail.com>
2021-03-09 16:53:21 +0200

    Provide the Theme Context to the App

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
+import { theme } from './src/infrastructure/theme';
 import RestaurantScreen from './src/features/restaurant/screens/RestaurantScreen';
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
