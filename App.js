136cf3ed5c5e5a3b1ad424f055a5df68a02f07c3
placiderapson <placideirandora@gmail.com>
2021-03-10 14:02:26 +0200

    Implement the Minimal Bottom Navigation with React Navigation

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import {
@@ -6,9 +7,32 @@ import {
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { theme } from './src/infrastructure/theme';
 import RestaurantScreen from './src/features/restaurant/screens/RestaurantScreen';
+import { SafeArea } from './src/components/utils/SafeArea';
+
+const content = { justifyContent: 'center', alignItems: 'center' };
+
+const MapScreen = () => {
+  return (
+    <SafeArea style={content}>
+      <Text>Maps!</Text>
+    </SafeArea>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <SafeArea style={content}>
+      <Text>Settings!</Text>
+    </SafeArea>
+  );
+};
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,7 +50,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+            <Tab.Screen name="Maps" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
