4b32dfbf769235d5a025700450a466d145cc8653
placiderapson <placideirandora@gmail.com>
2021-03-11 10:03:28 +0200

    Implement the Restaurants Request Service and Manage the Restaurants Global State in the Restaurant Context Provider Component

--- src/services/restaurant/mock/restaurantContext.js
+++ src/services/restaurant/mock/restaurantContext.js
@@ -1,13 +1,36 @@
-import React, { useState, createContext, useEffect, useMemo } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 import { restaurantsRequest, restaurantsTransform } from './restaurantService';
 
 export const RestaurantsContext = createContext();
 
-export const RestaurantsContextProvider = ({ children }) => (
-  <RestaurantsContext.Provider
-    value={{ restaurants: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }}
-  >
-    {children}
-  </RestaurantsContext.Provider>
-);
+export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchRestaurants = async () => {
+    try {
+      setLoading(true);
+
+      const data = await restaurantsRequest();
+      const transformedData = restaurantsTransform(data);
+
+      setRestaurants(transformedData);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  return (
+    <RestaurantsContext.Provider value={{ restaurants, loading, error }}>
+      {children}
+    </RestaurantsContext.Provider>
+  );
+};
