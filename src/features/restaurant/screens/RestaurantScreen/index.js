c0e600655c4a19e098cc06aa7da612587e3dc29f
placiderapson <placideirandora@gmail.com>
2021-03-11 11:02:11 +0200

    Display the Loading Spinner Natively on each Platform while Fetching for Restaurants in the API Call

--- src/features/restaurant/screens/RestaurantScreen/index.js
+++ src/features/restaurant/screens/RestaurantScreen/index.js
@@ -5,6 +5,7 @@ import RestaurantCardInfo from '../../components/RestaurantCardInfo';
 import {
   SearchContainer,
   RestaurantListContainer,
+  LoadingSpinner,
 } from './restaurantScreenElements';
 import { SafeArea } from '../../../../components/utils/SafeArea';
 import { RestaurantsContext } from '../../../../services/restaurant/mock/restaurantContext';
@@ -14,7 +15,7 @@ const RestaurantScreen = () => {
 
   const handleChangeSearch = (query) => setSearchQuery(query);
 
-  const { restaurants } = useContext(RestaurantsContext);
+  const { loading, restaurants } = useContext(RestaurantsContext);
 
   return (
     <>
@@ -22,11 +23,15 @@ const RestaurantScreen = () => {
         <SearchContainer>
           <Searchbar onChangeText={handleChangeSearch} value={searchQuery} />
         </SearchContainer>
-        <RestaurantListContainer
-          data={restaurants}
-          keyExtractor={(item) => item.name}
-          renderItem={({ item }) => <RestaurantCardInfo restaurant={item} />}
-        />
+        {loading ? (
+          <LoadingSpinner animating={true} color="#DB5A42" size="large" />
+        ) : (
+          <RestaurantListContainer
+            data={restaurants}
+            keyExtractor={(item) => item.name}
+            renderItem={({ item }) => <RestaurantCardInfo restaurant={item} />}
+          />
+        )}
       </SafeArea>
     </>
   );
--- src/features/restaurant/screens/RestaurantScreen/restaurantScreenElements.js
+++ src/features/restaurant/screens/RestaurantScreen/restaurantScreenElements.js
@@ -1,4 +1,4 @@
-import { FlatList } from 'react-native';
+import { FlatList, ActivityIndicator } from 'react-native';
 import styled from 'styled-components/native';
 
 export const SearchContainer = styled.View`
@@ -10,3 +10,9 @@ export const RestaurantListContainer = styled(FlatList).attrs({
     padding: 16,
   },
 })``;
+
+export const LoadingSpinner = styled(ActivityIndicator)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
