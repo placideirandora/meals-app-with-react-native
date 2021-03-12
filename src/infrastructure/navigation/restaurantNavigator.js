4e457419b27bf3063951aac79c857fd5719f93d5
placiderapson <placideirandora@gmail.com>
2021-03-12 15:13:19 +0200

    Implement the iOS  Modal Transition Approach for Transitioning from the Restaurant Screen to the Restaurant Detail Screen

--- src/infrastructure/navigation/restaurantNavigator.js
+++ src/infrastructure/navigation/restaurantNavigator.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 
 import RestaurantScreen from '../../features/restaurant/screens/RestaurantScreen';
 
 const RestaurantStack = createStackNavigator();
 
-const content = { flex: 1, justifyContent: 'center', alignItems: 'center' };
+const content = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#DB5A42',
+};
 
 const RestaurantNavigator = () => {
   return (
     <>
-      <RestaurantStack.Navigator headerMode="none">
+      <RestaurantStack.Navigator
+        headerMode="none"
+        screenOptions={{
+          ...TransitionPresets.ModalPresentationIOS,
+        }}
+      >
         <RestaurantStack.Screen
           name="Restaurants"
           component={RestaurantScreen}
