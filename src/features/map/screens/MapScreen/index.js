8e77f035a5c54d89f55246af19f29a3f0f0a4edc
placiderapson <placideirandora@gmail.com>
2021-03-12 17:44:54 +0200

    Create the Map Screen and Render the React Native Map

---
+++ src/features/map/screens/MapScreen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Map, MapContainer } from './mapElements';
+
+const MapScreen = () => {
+  return (
+    <>
+      <MapContainer>
+        <Map />
+      </MapContainer>
+    </>
+  );
+};
+
+export default MapScreen;
---
+++ src/features/map/screens/MapScreen/mapElements.js
@@ -0,0 +1,10 @@
+import MapView from 'react-native-maps';
+import styled from 'styled-components/native';
+
+export const MapContainer = styled.View`
+  flex: 1;
+`;
+
+export const Map = styled(MapView)`
+  height: 100%;
+`;
