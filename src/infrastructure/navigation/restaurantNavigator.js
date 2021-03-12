3487e92ecee3cc52e9c3175c005096cc3ef825a2
placiderapson <placideirandora@gmail.com>
2021-03-12 14:53:58 +0200

    Add a Minimal Restaurant Detail Screen

--- src/infrastructure/navigation/restaurantNavigator.js
+++ src/infrastructure/navigation/restaurantNavigator.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import RestaurantScreen from '../../features/restaurant/screens/RestaurantScreen';
 
 const RestaurantStack = createStackNavigator();
+
+const content = { flex: 1, justifyContent: 'center', alignItems: 'center' };
 
 const RestaurantNavigator = () => {
   return (
@@ -12,6 +15,14 @@ const RestaurantNavigator = () => {
         <RestaurantStack.Screen
           name="Restaurants"
           component={RestaurantScreen}
+        />
+        <RestaurantStack.Screen
+          name="RestaurantDetail"
+          component={() => (
+            <View style={content}>
+              <Text>Restaurant Detail</Text>
+            </View>
+          )}
         />
       </RestaurantStack.Navigator>
     </>
