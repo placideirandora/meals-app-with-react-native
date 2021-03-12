30ee3bcc1921f34019be48c33c0d55f6b6596a1c
placiderapson <placideirandora@gmail.com>
2021-03-12 16:31:14 +0200

    Add Restaurant Menu to the Restaurant Detail Screen

--- src/features/restaurant/screens/RestaurantDetailScreen/index.js
+++ src/features/restaurant/screens/RestaurantDetailScreen/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
+import { List } from 'react-native-paper';
 
 import { SafeArea } from '../../../../components/utils/SafeArea';
 import RestaurantCardInfo from '../../components/RestaurantCardInfo';
@@ -10,6 +12,41 @@ const RestaurantDetailScreen = ({ route }) => {
     <>
       <SafeArea>
         <RestaurantCardInfo restaurant={restaurant} />
+        <ScrollView>
+          <List.Section title="Menu">
+            <List.Accordion
+              title="Breakfast"
+              left={(props) => <List.Icon {...props} icon="bread-slice" />}
+            >
+              <List.Item title="Pizza" />
+              <List.Item title="Burger" />
+            </List.Accordion>
+
+            <List.Accordion
+              title="Lunch"
+              left={(props) => <List.Icon {...props} icon="pizza" />}
+            >
+              <List.Item title="Rice" />
+              <List.Item title="Chips" />
+            </List.Accordion>
+
+            <List.Accordion
+              title="Dinner"
+              left={(props) => <List.Icon {...props} icon="hamburger" />}
+            >
+              <List.Item title="Pork" />
+              <List.Item title="Kebabs" />
+            </List.Accordion>
+
+            <List.Accordion
+              title="Drinks"
+              left={(props) => <List.Icon {...props} icon="beer" />}
+            >
+              <List.Item title="Hennesy" />
+              <List.Item title="Tanqueray" />
+            </List.Accordion>
+          </List.Section>
+        </ScrollView>
       </SafeArea>
     </>
   );
